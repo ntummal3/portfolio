215c82952780b8964bc6ff0315722c94acc12758
Navyatej Tummala <ntummal3@ncsu.edu>
2025-05-12 18:57:17 -0400

    Updated mail password handling

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', '').replace(' ', '')  # Remove any spaces from password
 mail = Mail(app)
 
 # Your data
