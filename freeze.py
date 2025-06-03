4eda484e2a2e8ff31c285123a0018fbc2113f9f3
Navyatej Tummala <ntummal3@ncsu.edu>
2025-06-03 11:38:46 +0530

    Update GitHub Pages deployment configuration

--- freeze.py
+++
@@ -1,9 +0,0 @@
-from flask_frozen import Freezer
-from app import app
-
-freezer = Freezer(app)
-app.config['FREEZER_DESTINATION'] = 'build'
-app.config['FREEZER_RELATIVE_URLS'] = True
-
-if __name__ == '__main__':
-    freezer.freeze() 
